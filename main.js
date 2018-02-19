880054a42b2874400535d50a0d2ca626bba55206
Adam Tavener <a.tavvy@gmail.com>
2018-02-19 10:29:02 +0000

    bubble up errors in loading  🐿 v2.7.0

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ import { importClient } from './lib/utils';
 
 module.exports = {
 	SwgController,
-	swgLoader: (options) => new Promise(resolve => {
+	swgLoader: (options) => new Promise((resolve, reject) => {
 		const loadOptions = { manual: !!options.manualInitDomain };
 		SwgController.load(loadOptions).then(client => {
 			const swg = new SwgController(client, options);
 			resolve(swg);
-		});
+		})
+		.catch(reject);
 	}),
 	importClient
 };
