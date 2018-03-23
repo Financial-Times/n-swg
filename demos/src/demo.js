7e8df27b856dbd3a3775d910c98bba639ec6c4da
Adam Tavener <a.tavvy@gmail.com>
2018-03-23 10:04:47 +0000

    update circle.yml cache config  🐿 v2.7.0

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -9,7 +9,7 @@ const options = {
 	manualInitDomain: !!(document.querySelector('[data-n-swg-demo-manual-mode=true]')) ? 'ft.com:subscribed' : false,
 	sandbox: !!(document.querySelector('[data-n-swg-demo-env=sandbox]')),
 	subscribeFromButton: true,
-	M_SWG_SUB_SUCCESS_ENDPOINT: '/cors-endpoint/success',
+	M_SWG_SUB_SUCCESS_ENDPOINT: '/cors-endpoint/success'
 };
 
 console.log('SwG options:', JSON.stringify(options, null, 2));
