bbf1419f550e5341c5df3397c515d70b7a28fbb8
Adam Tavener <a.tavvy@gmail.com>
2018-03-23 09:25:55 +0000

    run demo in https  🐿 v2.7.0

--- demos/controllers/mock-endpoints.js
+++ demos/controllers/mock-endpoints.js
@@ -1,9 +1,50 @@
-const mockMembership = require('../utils/membership-mocks');
+const _get = require('lodash.get');
+const logger = require('@financial-times/n-logger').default;
+const SESSION_COOKIE_EXPIRY = 15552000000;
+
+const generateMockCookie = (res, { key, val }={}) => {
+	const cookie = {
+		path: '/',
+		domain: '.ft.com',
+		expires: new Date(Date.now() + SESSION_COOKIE_EXPIRY)
+	};
+	res.cookie(key, val, cookie);
+};
+
+const generateCookieData = ({ body }={}) => {
+	const purchaseData = _get(body, 'purchaseData');
+	if (!purchaseData) {
+		// entitlements success
+		return {
+			key: 'N_SWG_MOCK_ENTITLEMENT_SESSION_S',
+			val: _get(body, 'entitlements[0].subscriptionToken')
+		};
+	} else {
+		// purchase success
+		return {
+			key: 'N_SWG_MOCK_PURCHASE_SESSION_S',
+			val: purchaseData
+		};
+	}
+};
 
 module.exports = (MOCK_MODE) => (req, res, next) => {
-	if (MOCK_MODE) {
-		return mockMembership(req, res, next);
-	} else {
-		next();
+	if (!MOCK_MODE) return next();
+
+	/**
+	 * Mock Membership endpoints
+	 */
+	switch (req.params.result) {
+		case 'success':
+			logger.info('handling a proxied request', JSON.stringify(req.body, null, 2));
+			generateMockCookie(res, generateCookieData(req));
+			setTimeout(() => {
+				return res.status(201).json({
+					example: 'body'
+				});
+			}, 5000);
+			break;
+		default:
+			return next();
 	}
 };
