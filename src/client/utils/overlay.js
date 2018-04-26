84de1c0de154b08c326349a32558750c7baaff22
Remy Bach <remy@bach.me.uk>
2018-04-26 14:45:04 +0100

    Add close button to dismiss overlay
     🐿 v2.8.0

--- src/client/utils/overlay.js
+++ src/client/utils/overlay.js
@@ -2,21 +2,56 @@ module.exports = class Overlay {
 
 	constructor () {
 		this.el = document.createElement('div');
-		this.el.classList.add('o-overlay-shadow');
+		this.el.classList.add('overlay__shadow');
+
+		this.content = document.createElement('div');
+		this.content.classList.add('overlay__content');
+
+		this.inner = document.createElement('div');
+		this.inner.classList.add('overlay__inner');
+
+		this.close = document.createElement('a');
+		this.close.classList.add('overlay__close');
+
+		this.content.appendChild(this.close);
+		this.content.appendChild(this.inner);
+		this.el.appendChild(this.content);
+
+		this.visible = false;
+
+		this.bindEvents();
+	}
+
+	bindEvents () {
+		this.close.addEventListener('click', (e) => {
+			e.preventDefault();
+
+			this.hide();
+		});
+
+		document.body.addEventListener('keyup', (e) => {
+			if (this.visible && e.keyCode === 27) { // 27 = Escape
+				this.hide();
+			}
+		});
 	}
 
 	show (content) {
-		this.el.innerHTML = ''; //clear node
+		this.inner.innerHTML = ''; //clear node
 
 		if (content) {
-			this.el.innerHTML = `<div class="o-overlay-inner">${content}</div>`;
+			this.inner.innerHTML = content;
 		}
 
 		document.body.appendChild(this.el);
+
+		this.visible = true;
 	}
 
 	hide () {
 		document.body.removeChild(this.el);
+
+		this.visible = false;
 	}
 
 };
